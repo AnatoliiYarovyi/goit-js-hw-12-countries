a7701c0c6f89a8ae7094e798c1be65631e20fcea
Anatolii Yarovyi <apyarovyi@gmail.com>
2021-02-22 03:36:51 +0200

    Create refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+const refs = {
+    searchInput: document.querySelector('.js-search-input'),
+    countryContainer: document.querySelector('.js-country'),  
+    cleanerRef: document.querySelector('.cleaner'),
+};
+
+export default refs;
