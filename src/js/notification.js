cda69ed6af25003858fc60210cc10fbfeadaa617
Anatolii Yarovyi <apyarovyi@gmail.com>
2021-02-22 03:36:45 +0200

    Create notification.js

---
+++ src/js/notification.js
@@ -0,0 +1,33 @@
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/Material.css';
+import 'material-design-icons/iconfont/material-icons.css';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import {
+  notice,
+  error,
+  defaultModules,
+  // defaults,
+  Stack,
+} from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+defaultModules.set(PNotifyMobile, {});
+const myStack = new Stack({
+  dir1: 'down',
+  dir2: 'left',
+  firstpos1: 25,
+  firstpos2: 30,
+  context: document.body,
+});
+const noticeOptions = {
+  title: 'Serching for images',
+  text: 'Awaiting while downloading images from server',
+  stack: myStack,
+  delay: 2000,
+};
+// notice({
+//       ...noticeOptions,
+//       title: 'Ops! No more Images',
+//       text: 'Try to find something else!',
+// });
+export default error;
